bfe655b01d7c5c0d7e44e124ceea0c65e4dc17f8
ninobrown585 <adrian.brown1@g.austincc.edu>
2021-06-13 14:55:30 -0500

    Added indexDB

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
-const path = require("path")
+//const path = require("path")
 
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
